5203ec9b1f77c3347b1cac98888a917a10a539e9
skrooms <36216341+skrooms@users.noreply.github.com>
2018-06-28 16:57:40 -0400

    Added player and initial ground

---
+++ dodging-game/Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public Rigidbody rb;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(0, 0, 6000 * Time.deltaTime);
+	}
+}
